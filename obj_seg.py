122a81c7f37942df82d7874b66faf1fbcecedd22
radia78 <radia@Radis-MacBook-Pro.local>
2023-10-29 15:00:08 -0400

    Patch fix memory

--- obj_seg.py
+++ obj_seg.py
@@ -2,6 +2,7 @@ import torch
 from torchvision import transforms
 import numpy as np
 from torchvision.models.detection import ssdlite320_mobilenet_v3_large as SSDLite
+from torchvision.models.detection import SSDLite320_MobileNet_V3_Large_Weights
 import cv2
 from labels import COCO_INSTANCE_CATEGORY_NAMES as coco_names
 import ssl
@@ -13,9 +14,7 @@ def load_model():
     return model
 
 # preprocessing the image
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
+transform = SSDLite320_MobileNet_V3_Large_Weights.COCO_V1.transforms
 
 # the predict function
 def predict(image, model, detection_threshold):
